8e111c7bb35149576ff6e1878a4a73d507194275
Rajesh J <rajesh.j@octoze.com>
2026-04-26 02:33:12 +0530

    reactive form learned

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Form } from '../form/form';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Form],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/form/form.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, effect } from '@angular/core';
+import { FormControl, FormGroup, Validators,ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  imports: [ReactiveFormsModule,CommonModule],
+  templateUrl: './form.html',
+  styleUrl: './form.css',
+})
+export class Form {
+  form = new FormGroup({
+    name : new FormControl('',Validators.required),
+    age  : new FormControl('',[Validators.min(1),Validators.required]),
+    RollNo : new FormControl('',[Validators.required])
+  })
+
+  onSubmit(){
+    this.form.reset();// reset the form
+  }
+}
